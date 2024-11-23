8592caec5405d60efdbf35e0d4f5bd5e6165c95b
Dirga Febriyanda <dirgafebriyanda12@gmail.com>
2024-11-24 00:07:52 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,23 @@ window.onscroll = () => {
   menu.classList.remove("active");
   navlinks.classList.remove("show");
 };
+
+// Ambil elemen back-to-top
+const backToTopButton = document.querySelector(".back-to-top");
+
+// Fungsi untuk menangani scroll
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 700) {
+    // Jika posisi scroll lebih dari 200px
+    backToTopButton.style.opacity = 1; // Tampilkan tombol
+    backToTopButton.style.visibility = "visible";
+  } else {
+    backToTopButton.style.opacity = 0; // Sembunyikan tombol
+    backToTopButton.style.visibility = "hidden";
+  }
+});
+
+// Fungsi untuk kembali ke atas saat tombol diklik
+backToTopButton.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" }); // Scroll ke atas dengan animasi smooth
+});
